ad8b54147394cd428f6d944cecb1585df9e21f66
ahyun04 <ahyun125@shingu.g.ac.kr>
2023-10-12 11:09:05 +0900

    Feat : "타워 Target 발사 까지"

---
+++ My project2/Assets/scripts/TowerSystem/Projectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rd;
+
+    public float moveSpeed;
+    public float damagedAmount;
+    private bool hasDamaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        rd.velocity = transform.forward * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);
+    }
+
+}
